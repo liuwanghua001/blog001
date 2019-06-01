a2bbcfd0fe34dd5bdb00da3b977cbab6993f243c
pyaoling <275071120@qq.com>
2019-06-01 20:17:21 +0800

    django shell

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     # （路径名称，指向哪里）
     re_path('^index/$', views.index),
     re_path('^article/(?P<article_id>[0-9]+)$', views.article_page, name='article_page'),
-    re_path('^edit/$', views.edit_page, name='edit_page'),
+    re_path('^edit/(?P<article_id>[0-9]+)$', views.edit_page, name='edit_page'),
     re_path('^edit/action$', views.edit_action, name='edit_action')
     ]
